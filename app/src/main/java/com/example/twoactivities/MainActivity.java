83865789f2c1cbec72cc9fe91a33e8e15067b07f
nerbrain <malikmox@gmail.com>
2019-04-29 16:35:54 +0300

    First commit

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE ="com.example.android.twoactivities.extra.MESSAGE";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    
+    private EditText mMessageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, Second_activity.class);
+        String message = mMessageEditText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/twoactivities/Second_activity.java
@@ -0,0 +1,16 @@
+package com.example.twoactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second_activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activity);
+
+    }
+}
